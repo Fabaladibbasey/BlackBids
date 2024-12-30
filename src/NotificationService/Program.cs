a470d892036ca79a2dbaef027c465ff7475a3bc8
Fabala Dibbasey <fabaladibbasey27@gmail.com>
2024-12-30 02:04:37 +0000

    Add Notification Service with SignalR

---
+++ src/NotificationService/Program.cs
@@ -0,0 +1,41 @@
+using MassTransit;
+using NotificationService.Consumers;
+using NotificationService.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("nt", false));
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", h =>
+        {
+            h.Username(builder.Configuration.GetValue("RabbitMQ:Username", "guest")!);
+            h.Password(builder.Configuration.GetValue("RabbitMQ:Password", "guest")!);
+        });
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapHub<NotificationHub>("/notifications");
+
+app.Run();
+
